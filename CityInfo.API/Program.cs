4d0a48f83ec7d59be2ddbd5e227993004b6b69cb
Ícaro Levi Milet de Albuquerque Soares <i.albuquerque.soares@avanade.com>
2020-11-05 16:21:09 -0300

    Getting Acquainted with EF Core

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Contexts;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
 
@@ -21,7 +24,26 @@ namespace CityInfo.API
             try
             {
                 logger.Info("Initializing application...");
-                CreateWebHostBuilder(args).Build().Run();
+                var host = CreateWebHostBuilder(args).Build();
+
+                using (var scope = host.Services.CreateScope())
+                {
+                    try
+                    {
+                        var context = scope.ServiceProvider.GetService<CityInfoContext>();
+
+                        // For demo purposes, delete the database & migrate on startup so
+                        // we can start with a clean slate
+                        context.Database.EnsureDeleted();
+                        context.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex, "An error ocurred while migrating the database.");
+                    }
+                    // Run the web app
+                    host.Run();
+                }
             }
             catch (Exception ex)
             {
